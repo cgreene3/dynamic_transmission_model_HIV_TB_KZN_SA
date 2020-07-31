195a40bed8a8cfbbf043a84b63b6063dd9921351
jennifermross <jross3@uw.edu>
2020-07-31 14:44:33 -0700

    Added cause-groups for GBD calibration estimates

--- prep_gbd_prev_estimates.R
+++ prep_gbd_prev_estimates.R
@@ -15,13 +15,40 @@ outdir<-("~/GitHub/epi_model_HIV_TB/test_outputs/Jun25") #chelsea
 
 df<-read.csv(paste0(indir, "GBD_prev_1990_2017_jul28.csv"))
 
-#For each year (1990 to 2017) generate TB prevalence rate (per 100,000 population) by HIV+, HIV-, and male/female
+#Section 1 - Assembling calibration dataset. For each year (1990 to 2017) generate TB prevalence rate (per 100,000 population) by HIV+, HIV-, and male/female
 
-#Skeleton code that needs to be converted to apply functions. More to come here.
-#Active TB HIV negative male
+#Group 1 - Active TB among HIV-negative males. One value per year (1990 to 2017). Use the number in "val"
+    #For each year, sum across these three estimates: sex_id==1 & (cause_id==934|cause_id==946|cause_id==947)
+    #Use metric_id==3 to get rates and measure_id==5 for prevalence
 
-#Active TB prevalence HIV negative female
+#Group 2  - Active TB prevalence among HIV-negative females
+    #For each year, sum across these three estimates: sex_id==2 & (cause_id==934|cause_id==946|cause_id==947)
+    #Use metric_id==3 to get rates and measure_id==5 for prevalence
 
-#Active TB HIV+ male
+#Group 3 - Active TB HIV+ males
+    #For each year, sum across these three estimates: sex_id==1 & (cause_id==948|cause_id==949|cause_id==950)
+    #Use metric_id==3 to get rates and measure_id==5 for prevalence
 
-#Active TB HIV+ female
+#Group 4 - Active TB HIV+ females
+    #For each year, sum across these three estimates: sex_id==2 & (cause_id==948|cause_id==949|cause_id==950)
+    #Use metric_id==3 to get rates and measure_id==5 for prevalence
+
+
+
+#Section 2 - Line graph of values in groups 1-4 with years on x axis
+
+
+
+#Section 3 (later priority) - LTBI prevalence estimates over time
+
+#Group 5 - LTBI prevalence over time for males
+    #Use sex_id==1 and cause_id==954
+    #Use metric_id==3 to get rates and measure_id==5 for prevalence
+
+#Group 6 - LTBI prevalence over time for females
+    #Use sex_id==2 and cause_id==954
+    #Use metric_id==3 to get rates and measure_id==5 for prevalence
+
+
+
+#Section 4 - Line graph of values in groups 5&6 with years on x-axis. Values should be substantially higher than groups 1-4.
