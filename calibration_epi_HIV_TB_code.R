4bcae8155c2a0ed6e4ea5451e98b4ff150a7cac0
cgreene <cgreene3@uw.edu>
2020-08-03 12:16:57 -0700

    prevalence overtime graphs

--- calibration_epi_HIV_TB_code.R
+++ calibration_epi_HIV_TB_code.R
@@ -8,17 +8,12 @@ rm(list = ls())
 gc()
 
 #load packages
-sapply(c('dplyr', 'deSolve', 'readxl', 'stringr', 'reshape2', 'ggplot2', 'varhandle'), require, character.only=T)
+sapply(c('dplyr', 'deSolve', 'readxl', 'stringr', 'reshape2', 'ggplot2', 'varhandle', 'here', 'paletteer'), require, character.only=T)
 
-#Define input directory
-indir<-"~/ws/GitHub/ws.epi_model_HIV_TB/epi_model_HIV_TB/param_files/" #chelsea
-#indir<-("C:/Users/jross/repos/epi_model_HIV_TB/param_files/")
+indir<-'param_files/'
+outdir<-'test_outputs/calibration'
 
-#Define output directory - Set to outdir loc
-outdir<-("~/ws/GitHub/ws.epi_model_HIV_TB/test_outputs/calibration") #chelsea
-#outdir<- #jen
-
-df<-read.csv(paste0(indir, "GBD_prev_1990_2017_jul28.csv"))
+df<-read.csv(here(paste0(indir, "GBD_prev_1990_2017_jul28.csv")))
 
 #Section 1 - Assembling calibration dataset. For each year (1990 to 2017) generate TB prevalence rate (per 100,000 population) by HIV+, HIV-, and male/female
 
@@ -38,14 +33,6 @@ df<-read.csv(paste0(indir, "GBD_prev_1990_2017_jul28.csv"))
     #For each year, sum across these three estimates: sex_id==2 & (cause_id==948|cause_id==949|cause_id==950)
     #Use metric_id==3 to get rates and measure_id==5 for prevalence
 
-
-
-#Section 2 - Line graph of values in groups 1-4 with years on x axis
-
-
-
-#Section 3 (later priority) - LTBI prevalence estimates over time
-
 #Group 5 - LTBI prevalence over time for males
     #Use sex_id==1 and cause_id==954
     #Use metric_id==3 to get rates and measure_id==5 for prevalence
@@ -54,7 +41,82 @@ df<-read.csv(paste0(indir, "GBD_prev_1990_2017_jul28.csv"))
     #Use sex_id==2 and cause_id==954
     #Use metric_id==3 to get rates and measure_id==5 for prevalence
 
+df<-df%>%
+  mutate(group_id = if_else(sex_id == 1 & cause_id == 934|
+         sex_id == 1 & cause_id == 946|
+         sex_id == 1 & cause_id == 947, 1,
+         if_else(sex_id == 2 & cause_id == 934|
+         sex_id == 2 & cause_id == 946|
+         sex_id == 2 & cause_id == 947, 2,
+         if_else(sex_id == 1 & cause_id == 948|
+         sex_id == 1 & cause_id == 949|
+         sex_id == 1 & cause_id == 950, 3,
+         if_else(sex_id == 2 & cause_id == 948|
+         sex_id == 2 & cause_id == 949|
+         sex_id == 2 & cause_id == 950, 4,
+         if_else(sex_id == 1 & cause_id == 954, 5,
+                 if_else(sex_id == 2 & cause_id == 954, 6,
+                         100)))))))
 
+#filter to only include relevant values
+df<-df%>%
+  filter(group_id != 100)%>%
+  filter(measure_id == 5 & metric_id == 3)%>%
+  group_by(group_id, year)%>%
+  summarise(expected = sum(val),
+            upper = sum(upper),
+            lower = sum(lower))
+
+#Section 2 - Line graph of values in groups 1-4 with years on x axis
+
+df_active<-df%>%
+  filter(group_id == 1|
+           group_id == 2|
+           group_id == 3|
+           group_id == 4)
+
+df_active$compartment <- if_else(df_active$group_id == 1, 'HIV-, Male',
+                                 if_else(df_active$group_id == 2, 'HIV-, Female',
+                                         if_else(df_active$group_id == 3, 'HIV+, Male',
+                                                 'HIV+, Female'))) 
+
+df_active$group_id<-as.factor(df_active$group_id)
+
+active_prev_overtime <- ggplot(df_active, aes(x = year, 
+                                              y = expected,
+                                              group = compartment,
+                                              color = compartment))+
+  geom_line() +
+  geom_point()+
+  geom_errorbar(aes(ymin=lower, ymax=upper), width=.2,
+                 position=position_dodge(0.05))+
+  labs(title = "active prevalence overtime",
+       y = 'prevalence') +
+  scale_color_manual(values=c(paletteer_dynamic("cartography::green.pal", 4)))
+
+print(active_prev_overtime)
  
-#Section 4 - Line graph of values in groups 5&6 with years on x-axis. Values should be substantially higher than groups 1-4.
+#Section 3 - Line graph of values in groups 5&6 with years on x-axis. Values should be substantially higher than groups 1-4.
+
+df_ltbi<-df%>%
+  filter(group_id == 5|
+           group_id == 6)
+
+df_ltbi$group_name <- if_else(df_ltbi$group_id == 5, 'Male', 'Female')
+
+df_ltbi$group_id<-as.factor(df_ltbi$group_id)
+
+ltbi_prev_overtime <- ggplot(df_ltbi, aes(x = year, 
+                                              y = expected,
+                                              group = group_name,
+                                              color = group_name))+
+  geom_line() +
+  geom_point()+
+  geom_errorbar(aes(ymin=lower, ymax=upper), width=.2,
+                 position=position_dodge(0.05))+
+  labs(title = "LTBI prevalence overtime",
+       y = 'prevalence') +
+  scale_color_manual(values=c(paletteer_dynamic("cartography::blue.pal", 2)))
+
+print(ltbi_prev_overtime)
 
