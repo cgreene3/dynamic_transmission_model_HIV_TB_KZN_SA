ed7fa50323820b883fa78ffac1ac67a300da333e
cgreene <cgreene3@uw.edu>
2020-08-03 10:55:59 -0700

    renamed files

--- calibration_epi_HIV_TB_code.R
+++ calibration_epi_HIV_TB_code.R
@@ -3,14 +3,19 @@
 #Data are already
 #July 28, 2020
 
+#clean workspace
 rm(list = ls())
+gc()
+
+#load packages
+sapply(c('dplyr', 'deSolve', 'readxl', 'stringr', 'reshape2', 'ggplot2', 'varhandle'), require, character.only=T)
 
 #Define input directory
-#indir<-"~/ws/GitHub/ws.epi_model_HIV_TB/epi_model_HIV_TB/param_files/" #chelsea
-indir<-("C:/Users/jross/repos/epi_model_HIV_TB/param_files/")
+indir<-"~/ws/GitHub/ws.epi_model_HIV_TB/epi_model_HIV_TB/param_files/" #chelsea
+#indir<-("C:/Users/jross/repos/epi_model_HIV_TB/param_files/")
 
 #Define output directory - Set to outdir loc
-outdir<-("~/GitHub/epi_model_HIV_TB/test_outputs/Jun25") #chelsea
+outdir<-("~/ws/GitHub/ws.epi_model_HIV_TB/test_outputs/calibration") #chelsea
 #outdir<- #jen
 
 df<-read.csv(paste0(indir, "GBD_prev_1990_2017_jul28.csv"))
